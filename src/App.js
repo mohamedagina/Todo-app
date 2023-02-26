a8661b146e168bb24be65f1e26a804fadb01d8c5
mohamedagina <mohamedagina001@gmail.com>
2023-02-26 18:27:37 +0200

    feat: Add App Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import Header from './components/Header';
 function App() {
-  return <div className="App"></div>;
+  return (
+    <main>
+      <Header />
+    </main>
+  );
 }
 
 export default App;
