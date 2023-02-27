3c07c5efd02723908f5c516720d61bfcba0c2fa5
mohamedagina <mohamedagina001@gmail.com>
2023-02-27 19:17:45 +0200

    feat: Rendert TodoSection

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from './components/Header';
+import TodoSection from './components/TodoSection';
 function App() {
   return (
     <main>
       <Header />
+      <TodoSection />
     </main>
   );
 }
