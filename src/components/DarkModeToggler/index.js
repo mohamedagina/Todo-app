6a41df308f463e8dbda4ad3aaca0ca1c0dabc25b
mohamedagina <mohamedagina001@gmail.com>
2023-06-13 20:03:18 +0300

    feat: Integrate theme toggler with browser theme

--- src/components/DarkModeToggler/index.js
+++ src/components/DarkModeToggler/index.js
@@ -6,24 +6,52 @@ import moonIcon from '../../images/icon-moon.svg';
 import './DarkModeToggler.css';
 
 const DarkModeToggler = () => {
-  const [isDark, setIsDark] = useState(false);
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      setTheme(savedTheme);
+      return;
+    }
+
+    const darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    if (darkQuery.matches) setTheme('dark');
+    else setTheme('light');
+
+    darkQuery.onchange = e =>
+      e.matches ? setTheme('dark') : setTheme('light');
+
+    return () => (darkQuery.onchange = null);
+  }, []);
 
   useEffect(() => {
     const html = document.querySelector('html');
-    if (isDark) html.classList.add('dark');
-    else html.classList.remove('dark');
-  }, [isDark]);
+    if (theme === 'dark') {
+      html.classList.remove('light');
+      html.classList.add(theme);
+    } else {
+      html.classList.remove('dark');
+      html.classList.add(theme);
+    }
+  }, [theme]);
+
+  const handleToggleTheme = () => {
+    const newTheme = theme === 'dark' ? 'light' : 'dark';
+    setTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+  };
 
   return (
     <button
       className="icon-btn mode-toggler"
       type="button"
-      title={`Toggle ${isDark ? 'light' : 'dark'} mode`}
-      onClick={() => setIsDark(!isDark)}
+      title={`Toggle ${theme === 'dark' ? 'light' : 'dark'} mode`}
+      onClick={handleToggleTheme}
     >
       <span className="sr-only">Toggle Dark Mode</span>
 
-      <img src={isDark ? sunIcon : moonIcon} alt="" />
+      <img src={theme === 'dark' ? sunIcon : moonIcon} alt="" />
     </button>
   );
 };
